77121193ec3eea58fa8d8ca0e642330e931107e6
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-04-22 18:08:59 -0300

    save app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { Header } from '../components/Header'
 import '../styles/globals.scss'
 import styles from "../styles/app.module.scss";
 import { Player } from '../components/Player';
+
 function MyApp({ Component, pageProps }) {
   return (
     <div className={styles.wrapper}>
