276acbf41d734e5e466593480480b4a2af34861e
gabriel3421 <gabrielsouza.n.s@hotmail.com>
2021-04-26 10:05:39 -0300

    aula 04

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,8 @@ import { convertDurationToTimeString } from "../utils/convertDurationToTimeStrin
 import styles from './home.module.scss';
 import Image from 'next/image'
 import Link from 'next/link'
+import { useContext } from "react"
+import { PlayerContext } from "../contexts/PlayerContext"
 type Episode = {
   id: string;
   title: string;
@@ -20,7 +22,8 @@ type HomeProps = {
   allEpisodes: Episode[];
   latestEpisodes: Episode[];
 }
-export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+export default function Home({ latestEpisodes, allEpisodes }: HomeProps) { 
+  const { play } = useContext(PlayerContext)
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
@@ -46,7 +49,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                 <span>{episode.durationAsString}</span>
               </div>
 
-              <button>
+              <button type="button" onClick={() => play(episode)}>
                 <img src="/play-green.svg" alt="Tocar episódio"/>
               </button>
             </li>
